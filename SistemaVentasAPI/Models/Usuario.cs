a78f2e314e9ef74878fb04fd8921dc9d527b89c2
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-11-03 11:34:45 -0600

    Agregar archivos de proyecto.

---
+++ SistemaVentasAPI/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaVentasAPI.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; } = string.Empty;
+        public string Rol { get; set; } = string.Empty;
+    }
+}
