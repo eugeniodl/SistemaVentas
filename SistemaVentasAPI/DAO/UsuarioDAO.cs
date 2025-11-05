524fc506c4d77421fc2391e674333e414a1db888
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-11-04 18:13:21 -0600

    Conexión

---
+++ SistemaVentasAPI/DAO/UsuarioDAO.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+using SistemaVentasAPI.Data;
+using SistemaVentasAPI.Models;
+using System.Data;
+
+namespace SistemaVentasAPI.DAO
+{
+    public class UsuarioDAO
+    {
+        private readonly ConexionDB _conexion;
+
+        public UsuarioDAO(ConexionDB conexion)
+        {
+            _conexion = conexion;
+        }
+
+        public async Task<Usuario?> ValidarUsuarioAsync(string nombre, string contrasena)
+        {
+            using var cn = _conexion.ObtenerConexion();
+            await cn.OpenAsync();
+
+            using var cmd = new SqlCommand(Procedimientos.SP_VALIDAR_USUARIO, cn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            cmd.Parameters.AddWithValue("@NombreUsuario", nombre);
+            cmd.Parameters.AddWithValue("@Contrasena", contrasena);
+
+            using var dr = await cmd.ExecuteReaderAsync();
+            if (await dr.ReadAsync())
+            {
+                return new Usuario
+                {
+                    Id = Convert.ToInt32(dr["IdUsuario"]),
+                    NombreUsuario = dr["NombreUsuario"].ToString()!,
+                    Rol = dr["IdRol"].ToString()!
+                };
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SistemaVentasAPI/Data/ConexionDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace SistemaVentasAPI.Data
+{
+    public class ConexionDB
+    {
+        private readonly string _cadenaSQL;
+
+        public ConexionDB(IConfiguration configuration)
+        {
+            _cadenaSQL = configuration.GetConnectionString("CadenaSQL")!;
+        }
+
+        public SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(_cadenaSQL);
+        }
+    }
+}
