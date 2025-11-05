6a52047755e04c469ea3c94c400a8514ccfaa138
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-11-05 13:14:51 -0600

    Iniciando autenticación

--- SistemaVentasAPI/Models/Usuario.cs
+++ SistemaVentasAPI/Models/Usuario.cs
@@ -2,7 +2,7 @@
 {
     public class Usuario
     {
-        public int Id { get; set; }
+        public int IdUsuario { get; set; }
         public string NombreUsuario { get; set; } = string.Empty;
         public string Rol { get; set; } = string.Empty;
     }
